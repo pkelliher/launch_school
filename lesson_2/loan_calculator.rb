6f9b76b5789d0e7e7cb110c3ccf8c9d91e3f0644
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-03-17 18:02:01 -0700

    loan calc

---
+++ lesson_2/loan_calculator.rb
@@ -0,0 +1,11 @@
+puts "Welcome to the mortgage calculator, please enter your name to get started:"
+name = gets.chomp
+
+puts "Please enter the amount of your home loan:"
+loan_amount = gets.chomp.to_i
+
+puts "Please enter the Annual Percentage Rate (APR) of your loan:"
+apr = gets.chomp.to_i
+
+puts "Please enter the duration of your loan in years:"
+loan_duration = gets.chomp.to_i
