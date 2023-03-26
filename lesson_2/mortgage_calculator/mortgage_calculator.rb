8252fa141fd54447c2a71cc1090199408fe71b0a
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-03-26 16:58:59 -0700

    update dir

---
+++ lesson_2/mortgage_calculator/mortgage_calculator.rb
@@ -0,0 +1,87 @@
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def valid_number?(num)
+  /^-?\d+$/.match(num)
+end
+
+def number?(num)
+  valid_number?(num) || float?(num)
+end
+
+def float?(num)
+  /\d/.match(num) && /^-?\d*\.?\d*$/.match(num)
+end
+
+system 'clear'
+
+prompt(MESSAGES['welcome'])
+
+name = ''
+loop do
+  name = gets.chomp.capitalize
+
+  if name.empty?
+    prompt(MESSAGES['valid_name'])
+  else
+    break
+  end
+end
+
+prompt("Hello #{name}")
+
+loop do
+  loan_amount = ''
+  loop do
+    prompt(MESSAGES['loan_amt'])
+    loan_amount = gets.chomp.gsub(/[\s,]/, '')
+
+    if valid_number?(loan_amount)
+      break
+    else
+      prompt(MESSAGES['try_valid_number'])
+    end
+  end
+
+  apr = ''
+  loop do
+    prompt(MESSAGES['apr'])
+    apr = gets.chomp
+
+    if apr.empty? || apr.to_f.negative?
+      prompt(MESSAGES['pos_num_apr'])
+    else
+      break
+    end
+  end
+
+  loan_duration_years = ''
+  loop do
+    prompt(MESSAGES['loan_years'])
+    loan_duration_years = gets.chomp
+    if valid_number?(loan_duration_years)
+      break
+    else
+      prompt(MESSAGES['try_valid_number'])
+    end
+  end
+
+  annual_interest_rate = apr.to_f / 100
+  monthly_interest_rate = annual_interest_rate / 12
+  months = loan_duration_years.to_i * 12
+
+  monthly_payment = loan_amount.to_f *
+                    (monthly_interest_rate / 
+                    (1 - (1 + monthly_interest_rate)**-months))
+
+  prompt("Your monthly mortgage payment is: $#{format('%.2f', monthly_payment)}")
+  prompt(MESSAGES['try_again'])
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt(MESSAGES['thank_you'])
