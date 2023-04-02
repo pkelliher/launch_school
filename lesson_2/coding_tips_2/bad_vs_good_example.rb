17626470cddecf22e549b944e3825879884de07e
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-04-01 19:29:18 -0700

    test

--- lesson_2/coding_tips_2/bad_vs_good_example.rb
+++ lesson_2/coding_tips_2/bad_vs_good_example.rb
@@ -12,14 +12,22 @@
 
 # better
 
-name = ''
+# name = ''
 
-puts "Enter your name: "
-loop do
-  name = gets.chomp
-  break unless name.empty?
-  puts "That's an invalid name. Try again:"
+# puts "Enter your name: "
+# loop do
+#   name = gets.chomp
+#   break unless name.empty?
+#   puts "That's an invalid name. Try again:"
+# end
+
+# puts "Welcome #{name}!"
+# puts "What would you like to do?"
+
+name = 'johnson'
+
+test = ['kim', 'joe', 'sam'].each do |fname|
+  name = fname
 end
 
-puts "Welcome #{name}!"
-puts "What would you like to do?"
+puts test
