ca912a1be892065321b821e20e52b621ea5d7b24
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-04-12 19:45:41 -0700

    testing scope

--- lesson_2/quiz_notes/quiz_workspace.rb
+++ lesson_2/quiz_notes/quiz_workspace.rb
@@ -34,17 +34,27 @@
 #   saved_word
 # end
 
-def longest_word(sentence)
-  words = sentence.split
-  saved_word = words[0]
+# def longest_word(sentence)
+#   words = sentence.split
+#   saved_word = words[0]
 
-  words.each do |word|
-    if word.length >= saved_word.length
-      saved_word = word
-    end
-  end
+#   words.each do |word|
+#     if word.length >= saved_word.length
+#       saved_word = word
+#     end
+#   end
 
-  saved_word
+#   saved_word
+# end
+
+# puts longest_word("This is the day that will change your life")
+
+
+def extend_greeting(greeting)
+  greeting += " there"
 end
 
-puts longest_word("This is the day that will change your life")
+greeting = "hi"
+extend_greeting(greeting)
+
+puts greeting
