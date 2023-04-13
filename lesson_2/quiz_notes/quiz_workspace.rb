bb66f6cce7a01b8e0a039c0cdc3cbd2f32a1df74
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-04-12 19:17:47 -0700

    quiz_workspace

---
+++ lesson_2/quiz_notes/quiz_workspace.rb
@@ -0,0 +1,50 @@
+# def longest_word(sentence)
+#   words = sentence.split
+#   saved_word = words[0]
+
+#   words.each do |word|
+#     if word.length > saved_word.length
+#       saved_word = word
+#     end
+#   end
+
+#   saved_word
+# end
+
+# def longest_word(sentence)
+#   words = sentence.split
+#   saved_word = words.shift
+
+#   words.each do |word|
+#     if saved_word.length < word.length
+#       saved_word = word
+#     end
+#   end
+
+# def longest_word(sentence)
+#   words = sentence.split
+#   saved_word = words.shift
+
+#   words.each do |word|
+#     if word.length >= saved_word.length
+#       saved_word = word
+#     end
+#   end
+
+#   saved_word
+# end
+
+def longest_word(sentence)
+  words = sentence.split
+  saved_word = words[0]
+
+  words.each do |word|
+    if word.length >= saved_word.length
+      saved_word = word
+    end
+  end
+
+  saved_word
+end
+
+puts longest_word("This is the day that will change your life")
