7e33a23c28e5ed09069bf0ba9a6f4b4bdf4907db
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-04-16 17:38:47 -0700

    questions 1 & 2

---
+++ lesson_3/practice_problems_easy_1.rb
@@ -0,0 +1,10 @@
+# Question 1
+numbers = [1, 2, 2, 4]
+numbers.uniq
+
+puts numbers
+
+# Question 2
+advice = "Few things in life are as important as house training your pet dinosaur."
+advice.gsub!('important', 'urgent')
+puts advice
