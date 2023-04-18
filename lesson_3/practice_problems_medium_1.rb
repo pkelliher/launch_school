9cae1be44da50efc2bc6200262d9ebb288bf00ab
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-04-17 18:07:48 -0700

    while example

--- lesson_3/practice_problems_medium_1.rb
+++ lesson_3/practice_problems_medium_1.rb
@@ -1,2 +1,24 @@
 # Write a one-line program that creates the following output 10 times, with the subsequent line indented 1 space to the right:
-10.times { |number| puts (" " * number) + "The Flintstones Rock!" }
+# 10.times { |number| puts (" " * number) + "The Flintstones Rock!" }
+
+# def factors(number)
+#   divisor = number
+#   factors = []
+#   begin
+#     factors << number / divisor if number % divisor == 0
+#     divisor -= 1
+#   end until divisor == 0
+#   factors
+# end
+
+def factors(number)
+  divisor = number
+  factors = []
+  while divisor > 0
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end
+  factors
+end
+
+puts factors(25)
