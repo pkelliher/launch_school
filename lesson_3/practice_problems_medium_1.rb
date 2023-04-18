f7a089fcb8e813cda9ca762c48bca5d5abb664e5
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-04-17 17:49:04 -0700

    ascii art

---
+++ lesson_3/practice_problems_medium_1.rb
@@ -0,0 +1,2 @@
+# Write a one-line program that creates the following output 10 times, with the subsequent line indented 1 space to the right:
+10.times { |number| puts (" " * number) + "The Flintstones Rock!" }
