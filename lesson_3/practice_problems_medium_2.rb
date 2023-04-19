906771cd9575e39310e3842bbf3810082c22f408
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-04-18 19:01:16 -0700

    tricky 3

--- lesson_3/practice_problems_medium_2.rb
+++ lesson_3/practice_problems_medium_2.rb
@@ -10,14 +10,26 @@
 # puts "String_arg_one looks like this now: #{string_arg_one}"
 # puts "String_arg_two looks like this now: #{string_arg_two}"
 
-def tricky_method_two(a_string_param, an_array_param)
-  a_string_param << 'rutabaga'
-  an_array_param = ['pumpkins', 'rutabaga']
+# def tricky_method_two(a_string_param, an_array_param)
+#   a_string_param << 'rutabaga'
+#   an_array_param = ['pumpkins', 'rutabaga']
+# end
+
+# my_string = "pumpkins"
+# my_array = ["pumpkins"]
+# tricky_method_two(my_string, my_array)
+
+# puts "My string looks like this now: #{my_string}"
+# puts "My array looks like this now: #{my_array}"
+
+def tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param << "rutabaga"
 end
 
 my_string = "pumpkins"
 my_array = ["pumpkins"]
-tricky_method_two(my_string, my_array)
+tricky_method(my_string, my_array)
 
 puts "My string looks like this now: #{my_string}"
 puts "My array looks like this now: #{my_array}"
