485cdfa69f8585b4faf181f7cf19726342eb4208
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-04-18 18:47:37 -0700

    tricky method

--- lesson_3/practice_problems_medium_1.rb
+++ lesson_3/practice_problems_medium_1.rb
@@ -48,14 +48,14 @@
 
 # p answer - 8
 
-def rps(fist1, fist2)
-  if fist1 == "rock"
-    (fist2 == "paper") ? "paper" : "rock"
-  elsif fist1 == "paper"
-    (fist2 == "scissors") ? "scissors" : "paper"
-  else
-    (fist2 == "rock") ? "rock" : "scissors"
-  end
-end
+# def rps(fist1, fist2)
+#   if fist1 == "rock"
+#     (fist2 == "paper") ? "paper" : "rock"
+#   elsif fist1 == "paper"
+#     (fist2 == "scissors") ? "scissors" : "paper"
+#   else
+#     (fist2 == "rock") ? "rock" : "scissors"
+#   end
+# end
 
-puts rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock")
+# puts rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock")
