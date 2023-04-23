97ba091718d9ac6e9a0b3da9dd38470f55ed9102
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-04-23 16:18:37 -0700

    practice2

--- lesson_3/practice_problems_hard.rb
+++ lesson_3/practice_problems_hard.rb
@@ -36,3 +36,19 @@
 # puts "one is: #{one}"
 # puts "two is: #{two}"
 # puts "three is: #{three}"
+
+def mess_with_vars(one, two, three)
+  one.gsub!("one","two")
+  two.gsub!("two","three")
+  three.gsub!("three","one")
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
