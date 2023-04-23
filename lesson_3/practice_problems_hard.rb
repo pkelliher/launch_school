0e8e624cf31fabd6f4e212ca3007ae6c41e4df41
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-04-23 16:14:49 -0700

    practice problems

--- lesson_3/practice_problems_hard.rb
+++ lesson_3/practice_problems_hard.rb
@@ -1,6 +1,24 @@
-greetings = { a: 'hi' }
-informal_greeting = greetings[:a]
-informal_greeting << ' there'
+# greetings = { a: 'hi' }
+# informal_greeting = greetings[:a]
+# informal_greeting << ' there'
 
-puts informal_greeting  #  => "hi there"
-puts greetings
+# puts informal_greeting  #  => "hi there"
+# puts greetings
+
+
+
+def mess_with_vars(one, two, three)
+  one = two
+  two = three
+  three = one
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
