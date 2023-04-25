82763ec4d6f78f4c73b2371c9650c297f9d69d15
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2023-04-24 17:53:06 -0700

    practice

--- lesson_3/practice_problems_hard.rb
+++ lesson_3/practice_problems_hard.rb
@@ -37,21 +37,21 @@
 # puts "two is: #{two}"
 # puts "three is: #{three}"
 
-def mess_with_vars(one, two, three)
-  one.gsub!("one","two")
-  two.gsub!("two","three")
-  three.gsub!("three","one")
-end
+# def mess_with_vars(one, two, three)
+#   one.gsub!("one","two")
+#   two.gsub!("two","three")
+#   three.gsub!("three","one")
+# end
 
-one = "one"
-two = "two"
-three = "three"
+# one = "one"
+# two = "two"
+# three = "three"
 
-mess_with_vars(one, two, three)
+# mess_with_vars(one, two, three)
 
-puts "one is: #{one}"
-puts "two is: #{two}"
-puts "three is: #{three}"
+# puts "one is: #{one}"
+# puts "two is: #{two}"
+# puts "three is: #{three}"
 
 # def dot_separated_ip_address?(input_string)
 #   dot_separated_words = input_string.split(".")
@@ -62,14 +62,14 @@ puts "three is: #{three}"
 #   return true
 # end
 
-# def dot_separated_ip_address?(input_string)
-#   dot_separated_words = input_string.split(".")
-#   return false unless dot_separated_words.size == 4
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words.size == 4
 
-#   while dot_separated_words.size > 0 do
-#     word = dot_separated_words.pop
-#     return false unless is_an_ip_number?(word)
-#   end
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
 
-#   true
-# end
+  true
+end
